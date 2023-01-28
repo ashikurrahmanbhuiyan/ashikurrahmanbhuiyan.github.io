a528dfd9313c92c9c8be55a137b1556133149f1c
ashik <s2010376107@ru.ac.bd>
2023-01-28 14:33:39 +0600

    ok3

---
+++ contact-me/script.js
@@ -0,0 +1,8 @@
+document.getElementById('mail').addEventListener('click', function () {
+    var link = "mailto:ashikurrahmanbhuiyan@gmail.com"
+        + "?cc=" + encodeURIComponent(document.getElementById('email').value)
+        + "&subject=" + encodeURIComponent("From " + document.getElementById('name').value)
+        + "&body=" + encodeURIComponent(document.getElementById('myText').value);
+    window.location.href = link;
+    alert('Your message has been sent. Thank you!');
+});
