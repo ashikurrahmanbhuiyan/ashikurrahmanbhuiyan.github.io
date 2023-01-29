a988988262971b7d0136cf73fcc4e17b1faadfec
ashik <s2010376107@ru.ac.bd>
2023-01-29 13:40:36 +0600

    update docker

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
-FROM node:alpine
-COPY . /app
-CMD ["node", "/app/index.js"]
+FROM httpd:2.4
+COPY . /usr/local/apache2/htdocs/
+
+#docker build -t hello2/contact:1.0 .
+#docker run --rm -d -p 8080:80/tcp hello2/contact:1.0
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname })
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile('contact.html', { root: __dirname })
+})
+
+app.listen(port, () => {
+    console.log('Running.')
+})
